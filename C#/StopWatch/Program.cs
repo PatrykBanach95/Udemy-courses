103c639ac5a2ea3fe8203f01e7f64476ca4519df
PatrykBanach95 <48281807+PatrykBanach95@users.noreply.github.com>
2019-03-06 12:44:44 +0100

    Add files via upload

---
+++ C#/StopWatch/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = new StopWatch();
+            int choice;
+            do
+            {
+                Console.Write("Choose operation :\n 1 - Start the measurement\n 2 - Finish the measurement\n 3 - Result of measurement\n 0 - Close the program\n");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice) 
+                {
+
+                    case 1:
+                        stopWatch.Start();
+                        break;
+                    case 2:
+                        stopWatch.Stop();
+                        break;
+                    case 3:
+                        Console.WriteLine("The duration of the measurement is : " + stopWatch.Duration());
+                        break;
+
+                }
+                if(choice==0 || choice ==1 || choice ==2 || choice ==3)
+                {}
+                else
+                {
+                    Console.WriteLine("The wrong choice was made!");
+                }
+
+            } while (choice != 0);
+
+
+
+
+
+        }
+    }
+
+
+}
+
---
+++ C#/StopWatch/StopWatch.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+    public class StopWatch
+    {
+        private DateTime _start;
+        private DateTime _stop;
+        private bool _state;
+
+        public DateTime Start()
+        {
+            if (_state)
+            {
+                throw new InvalidOperationException("Please stop the last measurement");
+            }
+            _start = DateTime.Now;
+            _state = true;
+            return _start;
+        }
+        public DateTime Stop()
+        {
+            if (!_state)
+            {
+                throw new InvalidOperationException("First you should start measurement");
+            }
+            this._stop = DateTime.Now;
+            _state = false;
+            return _stop;
+        }
+        public TimeSpan Duration()
+        {
+            if (_state)
+            {
+                return DateTime.Now - _start;
+            }
+            else
+            {
+                return _stop - _start;
+            }
+        }
+    }
+}
