59562435ec0027afd1fdf1729e1fd5dff728f916
PatrykBanach95 <48281807+PatrykBanach95@users.noreply.github.com>
2019-03-07 16:50:08 +0100

    Add files via upload

---
+++ C#/Stack/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+            stack.Push("word");
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ C#/Stack/Stack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace stos
+{
+    public class Stack
+    {
+        private readonly List<object> _list = new List<object>();
+        public void Push(object obj)
+        {
+            if (obj == null)
+            {
+                throw new InvalidOperationException("Parameter cannot be null");
+            }
+            else
+            {
+                _list.Add(obj);
+            }
+            
+        }
+
+        public object Pop()
+        {            
+                var quantity = _list.Count();
+                if ( quantity == 0)
+                {
+                    throw new InvalidOperationException("Stack is empty");
+                }
+                else
+                {
+                    var last = _list[quantity - 1];
+                    _list.RemoveAt(quantity - 1);
+                    return last;
+                }             
+                        
+        }
+        public void Clear()
+        {
+            _list.Clear();
+        }
+
+    }
+}
